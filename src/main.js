14ce54f9ec9c65dadfe84ece0e1fbcc23a9d0379
pkrajczewski <krajczewskipawel@gmail.com>
2022-06-09 17:49:50 +0200

    initialize project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import store from './store'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
